43ef40315280b0bae55f61c16b2d65c559be9807
krackdian <andy.landaveri1848@gmail.com>
2025-11-29 16:31:48 -0500

    Crear Login

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,15 +2,30 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+  ],
+},
+
+
+  {
+    path: '/login',
+    component: () => import("src/components/auth/LoginForm.vue"),
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
+ {
+    path: '/register',
+    component: () => import("src/components/auth/RegisterForm.vue"),
+  },
+
+
+
+  
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
   },
 ]
+
 
 export default routes
